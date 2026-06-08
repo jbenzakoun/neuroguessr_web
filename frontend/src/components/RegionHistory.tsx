ab9580127dcaae1959200729dea1add73a7d4eeb
jbenzakoun <28443226+jbenzakoun@users.noreply.github.com>
2026-06-08 14:50:46 +0200

    improved layout for review mode

--- frontend/src/components/RegionHistory.tsx
+++ frontend/src/components/RegionHistory.tsx
@@ -240,7 +240,7 @@ const RegionHistory = ({pastRegions, niivue, highlightPastRegion, sessionName, s
                     </svg>
                   </span>
             }
-            <button
+            {/*<button
               className="highlight-region-button"
               onClick={(e) => { e.stopPropagation(); handleHighlightRegion(region); }}
               title={t("highlight_region")}
@@ -252,7 +252,7 @@ const RegionHistory = ({pastRegions, niivue, highlightPastRegion, sessionName, s
                 <line x1="12" y1="6" x2="12" y2="2"/>
                 <line x1="12" y1="22" x2="12" y2="18"/>
               </svg>
-            </button>
+            </button>*/}
           </div>
         ))}
       </div>
